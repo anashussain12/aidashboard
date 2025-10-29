0560811e04296ad62818cfbe734b7f5542af0fc0
Anas Hussain <hussainanas68@gmail.com>
2025-10-29 19:56:45 +0500

    new

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -41,7 +41,6 @@ const Navbar = () => {
         {/* Main Navbar Content */}
         {!isSidebarOpen && (
           <div className="flex items-center justify-between px-6 md:px-5 xl:px-28 py-4">
-            {/* Logo */}
             <div className="text-3xl font-black">
               <Link href="/">#aitoolcity</Link>
             </div>
