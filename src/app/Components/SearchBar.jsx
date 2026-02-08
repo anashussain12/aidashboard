73d7d4b61b4cd48be230afee9095520cf6fa7dc8
Anas Hussain <hussainanas68@gmail.com>
2026-02-08 20:31:44 +0500

    basant

--- src/app/Components/SearchBar.jsx
+++ src/app/Components/SearchBar.jsx
@@ -5,7 +5,6 @@ import React from "react";
 // import PropTypes from "pro";
 // import PropTypes from "pro";
 // import PropTypes from "pro";
-// import PropTypes from "pro";
 import { FaSearch } from "react-icons/fa";
 
 const categories = ["All", "Text", "Image", "Chatbots", "Code", "Video"];
