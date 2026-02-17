3c8fb21202842b56286a687c31013939109e9f4c
Anas Hussain <hussainanas68@gmail.com>
2026-02-17 07:18:27 +0500

    new

--- src/app/Components/SearchBar.jsx
+++ src/app/Components/SearchBar.jsx
@@ -3,7 +3,6 @@ import React from "react";
 // import PropTypes from "
 // import PropTypes from "
 // import PropTypes from "
-// import PropTypes from "
 import { FaSearch } from "react-icons/fa";
 
 const categories = ["All", "Text", "Image", "Chatbots", "Code", "Video"];
